f5243fb1091ebfd467e88504f665ecbb163021de
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-02-17 13:23:06 +0600

    ui design

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+
+document.getElementById('calculation-button').addEventListener('click', function(){
+
+// get income value
+    const incomeInputBalance = document.getElementById('income-input');
+    const incomeInputBalanceText = incomeInputBalance.value;
+    const amountValue = parseFloat(incomeInputBalanceText);
+    
+    //clear field
+      incomeInputBalance.value = ''; 
+
+
+
+    
+
+    // Expenses field total....
+
+const foodCostInput = document.getElementById('food-cost').value;
+console.log(foodCostInput);
+
+
+
+
+
+})
