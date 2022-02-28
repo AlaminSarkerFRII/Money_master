9c77324acedbf5dfefdd96f42e3bf9c9e9ba14c7
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-02-28 19:13:44 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -24,6 +24,20 @@ const clothsCostInput = getAmount('cloth-cost');
 const totalExpensesCost = parseFloat(foodCostInput) + parseFloat(clothsCostInput)+ parseFloat(rentCostText);
 
 console.log(totalExpensesCost);
+
+
+// set with condition = jodi khoroch besi hoy....
+
+if( amountValue < totalExpensesCost  ){
+    alert ('you cant expense this amount')
+}else {
+const totalCost = document.getElementById('total-expenses');
+const balanceText = document.getElementById('balance-total');
+const reminingBalance = amountValue - totalExpensesCost;
+totalCost.innerText = totalExpensesCost;
+balanceText.innerText = reminingBalance ;
+}
+
 //error handle on
 
  if(isNaN(parseFloat(incomeInputBalance)) || isNaN( parseFloat(foodCostInput)) || isNaN(parseFloat(clothsCostInput)) || isNaN (parseFloat(rentCostText))){
@@ -37,30 +51,46 @@ console.log(totalExpensesCost);
            
 //set in total Expenses
 
-const totalCost = document.getElementById('total-expenses');
+/* const totalCost = document.getElementById('total-expenses');
 const balanceText = document.getElementById('balance-total');
 const reminingBalance = amountValue - totalExpensesCost;
 totalCost.innerText = totalExpensesCost;
-balanceText.innerText = reminingBalance ;
+balanceText.innerText = reminingBalance ; */
 
 });
 
 document.getElementById('saving-button').addEventListener('click' , function(){
 
     const incomeBalance = getAmount('income-input')
+    console.log(incomeBalance);
 
     const savingInput = getAmount('saving-input');
+    console.log(savingInput);
     const savingAmount = document.getElementById('saving-amount');
     const remingBalance = document.getElementById('reming-balance');
 
-    const saveCalc = (parseFloat(incomeBalance)* parseFloat(savingInput))/100 ; 
 
-    savingAmount.innerText = saveCalc;
+     // check condition ... jodi saving besi hoy.........
+
+
+    if(incomeBalance <= savingInput){
+        alert('you cant save tihis amount')
+    } else {
+        const saveCalc = (parseFloat(incomeBalance)* parseFloat(savingInput))/100 ; 
+        savingAmount.innerText = saveCalc;
+        const balanceText = document.getElementById('balance-total');
+        const calcReminingBalance = parseFloat(balanceText.innerText ) - parseFloat(saveCalc)
+
+    remingBalance.innerText = calcReminingBalance; 
+    }
+
+    /* const saveCalc = (parseFloat(incomeBalance)* parseFloat(savingInput))/100 ; 
+    savingAmount.innerText = saveCalc; */
 
     // reming balance ........
-    const balanceText = document.getElementById('balance-total');
-const calcReminingBalance = parseFloat(balanceText.innerText ) - parseFloat(saveCalc)
+   /*  const balanceText = document.getElementById('balance-total');
+    const calcReminingBalance = parseFloat(balanceText.innerText ) - parseFloat(saveCalc)
 
-remingBalance.innerText = calcReminingBalance; 
+remingBalance.innerText = calcReminingBalance;  */
 
 })
